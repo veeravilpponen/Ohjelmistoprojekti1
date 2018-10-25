a2192606095a5f57c00088e27063f7fa618464cc
Vilpponen Veera <a1704520@myy.haaga-helia.fi>
2018-10-25 09:25:17 +0300

    First commit

---
+++ kysely/src/main/java/springpower/kysely/KyselyApplication.java
@@ -0,0 +1,12 @@
+package springpower.kysely;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyselyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KyselyApplication.class, args);
+	}
+}
