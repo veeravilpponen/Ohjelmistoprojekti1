1d75dbffea26c135844280b1da5fe0fbeb303ba2
Suvi Vappula <suvi.vappula@myy.haaga-helia.fi>
2018-10-25 10:22:22 +0300

    rest repoon

--- kysely/src/main/java/springpower/kysely/domain/KysymysRepository.java
+++ kysely/src/main/java/springpower/kysely/domain/KysymysRepository.java
@@ -1,7 +1,9 @@
 package springpower.kysely.domain;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface KysymysRepository extends CrudRepository <Kysymys, Long> {
 
 
