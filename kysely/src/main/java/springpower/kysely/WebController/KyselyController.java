8d9cb596d13d129c6ff89fae3c198db749f61a79
Mäkinen Max <a1704463@myy.haaga-helia.fi>
2018-10-29 09:00:42 +0200

    Added base-level URL mapping

--- kysely/src/main/java/springpower/kysely/WebController/KyselyController.java
+++ kysely/src/main/java/springpower/kysely/WebController/KyselyController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -30,7 +31,7 @@ public class KyselyController {
     } 
     
     /** palauttaa infosivun **/
-	@RequestMapping(value="/info")
+	@GetMapping({"/info", "/"})
 	public String info(Model model) {
 		
 		return "info";
