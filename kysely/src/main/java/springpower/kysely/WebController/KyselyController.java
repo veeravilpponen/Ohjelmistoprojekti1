29241160250d7c82b8e00b36793ff20a34e0d753
Mäkinen Max <a1704463@myy.haaga-helia.fi>
2018-11-01 10:50:51 +0200

    Added a branch for dev and enabled CORS on KyselyController

--- kysely/src/main/java/springpower/kysely/WebController/KyselyController.java
+++ kysely/src/main/java/springpower/kysely/WebController/KyselyController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,7 +16,7 @@ import springpower.kysely.domain.KysymysRepository;
 
 
 
-
+@CrossOrigin
 @Controller
 public class KyselyController {
 	
