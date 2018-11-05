3a2096e83828cf39d3fc6e5f48529b1290042296
Nurmi Maiju <a1704485@myy.haaga-helia.fi>
2018-11-05 09:54:29 +0200

    added @OneToMany ans @Many To One to Kyselu and kysymys

--- kysely/src/main/java/springpower/kysely/domain/Kysely.java
+++ kysely/src/main/java/springpower/kysely/domain/Kysely.java
@@ -1,18 +1,62 @@
 package springpower.kysely.domain;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+
 
 @Entity
 public class Kysely {
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
-	private Long id;
+	private Long kyselyId;
 	
+	private String kyselyNimi;
+	
+	@JsonIgnore
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "kysely")
+	private List<Kysymys> kysymykset;
+
 	public Kysely() {
-		super();
+		
 	}
+	
+	public Kysely(String kyselyNimi) {
+		super();
+		this.kyselyNimi = kyselyNimi;
+	}
+	
+	public String getKyselyNimi() {
+		return kyselyNimi;
+	}
+
+	public void setKyselyNimi(String kyselyNimi) {
+		this.kyselyNimi = kyselyNimi;
+	}
+
+	public List<Kysymys> getKysymykset() {
+		return kysymykset;
+	}
+
+	public void setKysymykset(List<Kysymys> kysymykset) {
+		this.kysymykset = kysymykset;
+	}
+
+	@Override
+	public String toString() {
+		return "Kysely [kyselyId=" + kyselyId + ", kyselyNimi=" + kyselyNimi + ", kysymykset=" + kysymykset + "]";
+	}
+
+	
+	
+	
 }
