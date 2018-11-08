bce3e2f9a713499cf20ea1d017edeb61e81b48ce
Kvartti <a1704516@myy.haaga-helia.fi>
2018-11-08 09:25:06 +0200

    kyselyiden listaus, metodi

--- kysely/src/main/java/springpower/kysely/WebController/KyselyController.java
+++ kysely/src/main/java/springpower/kysely/WebController/KyselyController.java
@@ -1,9 +1,19 @@
 package springpower.kysely.WebController;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import springpower.kysely.domain.Kysely;
+import springpower.kysely.domain.KyselyRepository;
+import springpower.kysely.domain.Kysymys;
 
 
 
@@ -20,5 +30,13 @@ public class KyselyController {
 		
 		return "info";
 	}
+	
+	@Autowired
+	private KyselyRepository kyselyRepository;
+	
+	@RequestMapping(value="/kyselyt", method = RequestMethod.GET)
+    public @ResponseBody List<Kysely> kyselyListRest() {	
+        return (List<Kysely>) kyselyRepository.findAll();
+    } 
 
 }
