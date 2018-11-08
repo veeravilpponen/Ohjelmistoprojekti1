aade14060252e766caa5a9f1869b2ee0429c09fb
Suvi Vappula <suvi.vappula@myy.haaga-helia.fi>
2018-11-08 09:19:36 +0200

    vastaus-luokan konstruktorit ja yhteys kysymykseen

--- kysely/src/main/java/springpower/kysely/domain/Vastaus.java
+++ kysely/src/main/java/springpower/kysely/domain/Vastaus.java
@@ -4,6 +4,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Vastaus {
@@ -13,6 +17,20 @@ public class Vastaus {
 	private Long id;
 	
 	private String vastausSisus;
+	
+	 @ManyToOne
+	 @JsonIgnore
+	 @JoinColumn(name = "vastausId")
+	 private Kysymys kysymys;
+	
+	public Vastaus() {
+		super();
+		this.vastausSisus=null;
+	}
+	public Vastaus(String vastausSisus) {
+		super();
+		this.vastausSisus=vastausSisus;
+	}
 	
 	
 	@Override
