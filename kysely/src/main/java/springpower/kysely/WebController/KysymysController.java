51c2fda987b2b915823ea51644db56474ee865a2
Suvi Vappula <suvi.vappula@myy.haaga-helia.fi>
2018-11-19 10:06:53 +0200

    linkit oikein

--- kysely/src/main/java/springpower/kysely/WebController/KysymysController.java
+++ kysely/src/main/java/springpower/kysely/WebController/KysymysController.java
@@ -27,10 +27,10 @@ public class KysymysController {
     } 
 	
 	/** näyttää  kysymyksen vastauksineen kysymyksen Id:n perusteella **/
+
 	@RequestMapping(value="/kysymykset/{kysymysId}/vastaukset", method = RequestMethod.GET)
     public @ResponseBody Optional<Kysymys> naytaKysymyksenVastaukset(@PathVariable("kysymysId") Long kysymysId)  {	
 
-		
 		Kysymys kysymys = kysymysRepository.findById(kysymysId).get();
 		System.out.println("KYSYMYS" + kysymys);
 		
