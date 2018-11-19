5c7d7905d38187cda19e53c3255deee8098d17fb
Suvi Vappula <suvi.vappula@myy.haaga-helia.fi>
2018-11-19 09:56:55 +0200

    Branch for war

--- kysely/src/main/java/springpower/kysely/KyselyApplication.java
+++ kysely/src/main/java/springpower/kysely/KyselyApplication.java
@@ -5,6 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 import springpower.kysely.domain.Kysely;
@@ -15,8 +17,13 @@ import springpower.kysely.domain.Vastaus;
 import springpower.kysely.domain.VastausRepository;
 
 @SpringBootApplication
-public class KyselyApplication {
+public class KyselyApplication extends SpringBootServletInitializer {
 	private static final Logger log = LoggerFactory.getLogger(KyselyApplication.class);
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	return application.sources(KyselyApplication.class);
+	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(KyselyApplication.class, args);
