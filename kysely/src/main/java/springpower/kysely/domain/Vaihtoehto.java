d1d495f78a2fb726001dda5846700de91b5d5612
Nurmi Maiju <a1704485@myy.haaga-helia.fi>
2018-11-22 10:07:38 +0200

    vaihtoehto.java

---
+++ kysely/src/main/java/springpower/kysely/domain/Vaihtoehto.java
@@ -0,0 +1,48 @@
+package springpower.kysely.domain;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+public class Vaihtoehto {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long vaihtoehtoId;
+	
+	private String vaihtoehtoSisus;
+	
+	 @ManyToOne
+	 @JoinColumn(name = "kysymysId")
+	 @JsonBackReference
+	 private Kysymys kysymys;
+	 
+		public Vaihtoehto() {
+			super();
+			this.setVaihtoehtoSisus(null);
+		}
+		
+		public Vaihtoehto(String vaihtoehtoSisus) {
+			super();
+			this.setVaihtoehtoSisus(vaihtoehtoSisus);
+		}
+		
+		public Vaihtoehto(String vaihtoehtoSisus, Kysymys kysymys) {
+			super();
+			this.setVaihtoehtoSisus(vaihtoehtoSisus);
+			this.kysymys = kysymys;
+		}
+
+		public String getVaihtoehtoSisus() {
+			return vaihtoehtoSisus;
+		}
+
+		public void setVaihtoehtoSisus(String vaihtoehtoSisus) {
+			this.vaihtoehtoSisus = vaihtoehtoSisus;
+		}
+
+}
