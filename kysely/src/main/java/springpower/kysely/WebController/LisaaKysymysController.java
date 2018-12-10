624de256d749c627c04c11fa7474f805be81a394
Suvi Vappula <suvi.vappula@myy.haaga-helia.fi>
2018-12-10 08:24:14 +0200

    lomakeyritys

--- kysely/src/main/java/springpower/kysely/WebController/LisaaKysymysController.java
+++ kysely/src/main/java/springpower/kysely/WebController/LisaaKysymysController.java
@@ -1,6 +1,7 @@
 package springpower.kysely.WebController;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,6 +11,7 @@ import springpower.kysely.domain.Kysymys;
 import springpower.kysely.domain.KysymysRepository;
 import springpower.kysely.domain.VaihtoehtoRepository;
 
+@Controller
 public class LisaaKysymysController {
 	
 	@Autowired
--- kysely/src/test/java/springpower/kysely/KyselyApplicationTests.java
+++ kysely/src/test/java/springpower/kysely/KyselyApplicationTests.java
@@ -1,4 +1,4 @@
-package springpower.kysely;
+/*package springpower.kysely;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,3 +14,4 @@ public class KyselyApplicationTests {
 	}
 
 }
+*/
